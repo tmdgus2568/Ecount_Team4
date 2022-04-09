f43f60a498258a516f9e6cc184068154cea41e46
Jung-woo Lee <you@example.com>
2022-04-09 12:52:55 +0900

    custom music player

---
+++ jungwoo/project1/js/data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'static/musics/1.mp3',
+        artist:'artist 1',
+        cover:'static/images/1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'static/musics/2.mp3',
+        artist:'artist 2',
+        cover:'static/images/2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'static/musics/3.mp3',
+        artist:'artist 3',
+        cover:'static/images/3.png'
+    }
+]
