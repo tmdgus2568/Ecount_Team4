3643bad148b4c9b80ec7d0b326e87bd2266cded7
민성 김 <ms930524@naver.com>
2022-04-10 01:35:18 +0900

    4.10 오디오 버튼 수정

---
+++ minseong/haircut(MS)/js/data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'She',
+        path: 'static/musics/She.mp3',
+        artist:'Jannabi',
+        cover:'https://i.postimg.cc/bNCpmr9d/she.jpg'
+    },
+    {
+        name: 'Shiny Star',
+        path: 'static/musics/ShinyStar.mp3',
+        artist:'KyoungSeo',
+        cover:'https://i.postimg.cc/5yPdKKvD/image.jpg'
+    },
+    {
+        name: 'OldSong',
+        path: 'static/musics/OldSong.mp3',
+        artist:'StandingEgg',
+        cover:'https://i.postimg.cc/7Z26xhGb/OldSong.jpg'
+    }
+]
